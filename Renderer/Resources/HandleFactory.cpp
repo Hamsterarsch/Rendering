c5ebcab6b52b6eb859f38cd2bf4bb647b5ccb84b
Robert Bellin <rl-bellin@web.de>
2020-02-24 13:50:01 +0100

    implemented handle retirement and reuse for handle factory

--- Renderer/Resources/HandleFactory.cpp
+++ Renderer/Resources/HandleFactory.cpp
@@ -3,13 +3,80 @@
 
 namespace Renderer
 {
+	HandleBucket::HandleBucket() :
+		size{ 0 }
+	{
+	}
+
+
+	
+	ResourceHandle HandleBucket::PopHandle()
+	{
+		decltype(items)::value_type item{ std::move(items.front()) };
+
+		items.pop_front();
+		--size;
+
+		return item;
+		
+	}
+
+	
+	
+	void HandleBucket::PushHandle(const ResourceHandle &handle)
+	{
+		items.push_front(handle);
+		++size;
+				
+	}
+
+
+	
+	size_t HandleBucket::Size() const
+	{
+		return size;
+		
+	}
+
+
+	
+	bool HandleBucket::IsNotEmpty() const
+	{
+		return !IsEmpty();
+		
+	}
+	
+		bool HandleBucket::IsEmpty() const
+		{
+			return size <= 0;
+			
+		}
+
+	
+
+	
 	ResourceHandle HandleFactory::MakeHandle(const ResourceHandle::t_resourceTypes type)
 	{
-		std::lock_guard<std::mutex> lock{ mutex };
+		{
+			std::lock_guard<std::mutex> lock{ mutexRetiredHandles };
+			if(retiredHandles[type].IsNotEmpty())
+			{
+				return retiredHandles[type].PopHandle();
+			}
+		}
 		
+		std::lock_guard<std::mutex> lock{ mutexSerialFactory };		
 		return ResourceHandle{ type, serialFactories[type].GetNextSerial() };
 				
 	}
-	
+
+	void HandleFactory::RetireHandle(const ResourceHandle &handle)
+	{
+		std::lock_guard<std::mutex> lock{ mutexRetiredHandles };
+		
+		retiredHandles[handle.GetResourceType()].PushHandle(handle);		
+				
+	}
+
 	
 }
--- Renderer/Resources/HandleFactory.hpp
+++ Renderer/Resources/HandleFactory.hpp
@@ -3,18 +3,50 @@
 #include "Resources/ResourceHandle.hpp"
 #include <unordered_map>
 #include <mutex>
+#include <forward_list>
 
 
 namespace Renderer
 {
+	class HandleBucket
+	{		
+		private: std::forward_list<ResourceHandle> items;
+
+		private: size_t size;
+				 					
+
+		
+		public: HandleBucket();
+
+		public: ResourceHandle PopHandle();
+						
+		public: void PushHandle(const ResourceHandle &handle);
+
+		public: size_t Size() const;
+
+		public: bool IsNotEmpty() const;
+
+		public: bool IsEmpty() const;
+				
+	};
+
+
+	
 	class HandleFactory
-	{
+	{	
 		private: std::unordered_map<ResourceHandle::t_resourceTypes, SerialFactory> serialFactories;
 
-		private: std::mutex mutex;
+		private: std::unordered_map<ResourceHandle::t_resourceTypes, HandleBucket> retiredHandles;
+		
+		private: std::mutex mutexSerialFactory;
 
+		private: std::mutex mutexRetiredHandles;
+				 
+				 		
 
 		public: ResourceHandle MakeHandle(ResourceHandle::t_resourceTypes type);
+
+		public: void RetireHandle(const ResourceHandle &handle);
 		
 	};
 
