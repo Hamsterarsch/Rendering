3b001cee4719f74eeb379e0e9b7b41d54782dea1
Robert Bellin <rl-bellin@web.de>
2020-05-06 16:08:32 +0200

    implemented a render surface class that can wrap valid or invalid render targets/depth targets, to use as render targets

---
+++ Renderer/Resources/RenderSurface.cpp
@@ -0,0 +1,96 @@
+#include "Resources/RenderSurface.hpp"
+#include "RHA/Interface/DX12/CmdList.hpp"
+
+
+namespace Renderer
+{
+	namespace DX12
+	{
+		RenderSurface::RenderSurface(RHA::DX12::WindowSurface *const colorSurface, RHA::DX12::DepthSurface *const depthSurface)
+			:
+			surfaceColor{ colorSurface },
+			surfaceDepth{ depthSurface }
+		{
+		}
+
+		
+
+		void RenderSurface::RecordSurfacePreparations(RHA::DX12::CmdList &list)
+		{
+			RecordClearDepthSurface(list);
+
+			if(ColorSurfaceIsVaild())
+			{
+				surfaceColor->RecordPreparationForRendering(list);				
+			}
+			RecordBindSurfaces(list);
+			
+		}
+		
+			void RenderSurface::RecordClearDepthSurface(RHA::DX12::CmdList &list) const
+			{
+				if(DepthSurfaceIsValid())
+				{					
+					list.RecordClearDsv(surfaceDepth->GetHandleCpu(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1, 0, 0, nullptr);					
+				}
+			
+			}
+			
+			void RenderSurface::RecordBindSurfaces(RHA::DX12::CmdList &list) const
+			{
+				if(ColorSurfaceIsVaild())
+				{
+					RecordBindColorAndDepthSurface(list);	
+				}
+				else if(DepthSurfaceIsValid())
+				{
+					RecordBindDepthSurface(list);
+				}
+			
+			}
+
+				void RenderSurface::RecordBindColorAndDepthSurface(RHA::DX12::CmdList &list) const
+				{
+					auto dsv{ DepthSurfaceIsValid() ? surfaceDepth->GetHandleCpu() : nullptr };
+					surfaceColor->RecordPipelineBindings
+					(
+						list,
+						&dsv
+					);
+			
+				}
+
+				void RenderSurface::RecordBindDepthSurface(RHA::DX12::CmdList &list) const
+				{
+					auto dsv{ surfaceDepth->GetHandleCpu() };
+					list.RecordSetRenderTargets(0, nullptr, false, &dsv);					
+								
+				}
+		
+		
+
+		void RenderSurface::RecordPresentPreparations(RHA::DX12::CmdList &list)
+		{
+			if(ColorSurfaceIsVaild())
+			{
+				surfaceColor->RecordPreparationForPresenting(list);					
+			}
+			
+		}
+
+		
+
+		void RenderSurface::Present()
+		{
+			if(ColorSurfaceIsVaild())
+			{
+				surfaceColor->Present();				
+			}
+			
+		}
+
+		
+	}
+
+	
+}
