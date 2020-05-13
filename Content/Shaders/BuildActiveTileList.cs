17a94ed4957799c3bb96569bd02a60402bc309ea
Robert Bellin <rl-bellin@web.de>
2020-05-13 20:02:05 +0200

    fixed shader bugs

--- Content/Shaders/BuildActiveTileList.cs
+++ Content/Shaders/BuildActiveTileList.cs
@@ -1,11 +1,11 @@
-ConstantBuffer<uint> tileCount : register(b1);
-RWBuffer<uint> volumeTileFlags : register(u1);
-AppendStructuredBuffer<uint> activeTileList : register(u2);
+cbuffer localConstantBuffer : register(b1) { uint tileCount; }
+Buffer<uint> volumeTileFlags : register(t0);
+AppendStructuredBuffer<uint> activeTileList : register(u1);
 
 [numthreads(32, 1, 1)]
 void main(uint3 dispatchID : SV_DispatchThreadID)
 {
-	if(dispatch.ID < tileCount && activeTileList[dispatchID.x])
+	if(dispatchID.x < tileCount && volumeTileFlags[dispatchID.x])
 	{
 		activeTileList.Append(dispatchID.x);
 	}
