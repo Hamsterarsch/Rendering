9599217d3bfe546aa4dfe94d31a30f06917bdb24
Robert Bellin <rl-bellin@web.de>
2020-05-14 14:48:44 +0200

    gpu validation is now enabled if debug layers are specified to be enabled. new nested namespace

--- RHA/DX12/DeviceResourcesImpl.cpp
+++ RHA/DX12/DeviceResourcesImpl.cpp
@@ -2,106 +2,105 @@
 #include "DX12/DeviceResourcesImpl.hpp"
 #include "Shared/Exception/CreationFailedException.hpp"
 
-namespace RHA
+namespace RHA::DX12
 {
-	namespace DX12
+	DeviceResourcesImpl::DeviceResourcesImpl(const D3D_FEATURE_LEVEL minimumFeatureLevel, const bool shouldEnableDebugLayers) :
+		minimumFeatureLevel{ minimumFeatureLevel }
 	{
-		DeviceResourcesImpl::DeviceResourcesImpl(const D3D_FEATURE_LEVEL minimumFeatureLevel, const bool shouldEnableDebugLayers) :
-			minimumFeatureLevel{ minimumFeatureLevel }
+		if(shouldEnableDebugLayers)
 		{
-			if(shouldEnableDebugLayers)
+			EnableDebugLayers();
+		}
+		
+		const auto result
+		{
+			CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory))
+		};
+		CheckDxgiFactoryCreation(result);
+					
+		CreateDeviceForFeatureLevel();
+				   
+	}
+
+		void DeviceResourcesImpl::EnableDebugLayers()
+		{
+			DxPtr<ID3D12Debug> debugController;
+			DxPtr<ID3D12Debug1> debugController1;
+			auto result
 			{
-				EnableDebugLayers();
-			}
-			
-			const auto result
-			{
-				CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory))
+				D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))
 			};
-			CheckDxgiFactoryCreation(result);
-						
-			CreateDeviceForFeatureLevel();
-					   
+			CheckDebugControllerCreation(result);
+
+			result = debugController->QueryInterface(IID_PPV_ARGS(&debugController1));
+			CheckDebugControllerCreation(result);
+
+			debugController->EnableDebugLayer();
+			debugController1->SetEnableGPUBasedValidation(true);
+		
 		}
 
-			void DeviceResourcesImpl::EnableDebugLayers()
-			{
-				DxPtr<ID3D12Debug> debugController;
-				const auto result
-				{
-					D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))
-				};
-
-				CheckDebugControllerCreation(result);
-
-				debugController->EnableDebugLayer();
-			
-			}
-
-				void DeviceResourcesImpl::CheckDebugControllerCreation(const HRESULT result) 
-				{
-					if (FAILED(result))
-					{
-						throw Exception::CreationFailed{ "Could not create debug interface for dx12 device resources" };
-					}
-				
-				}
-
-			void DeviceResourcesImpl::CheckDxgiFactoryCreation(const HRESULT result)
+			void DeviceResourcesImpl::CheckDebugControllerCreation(const HRESULT result) 
 			{
 				if (FAILED(result))
 				{
-					throw Exception::CreationFailed{ "Could not query dxgi factory" };
+					throw Exception::CreationFailed{ "Could not create debug interface for dx12 device resources" };
 				}
+			
+			}
+
+		void DeviceResourcesImpl::CheckDxgiFactoryCreation(const HRESULT result)
+		{
+			if (FAILED(result))
+			{
+				throw Exception::CreationFailed{ "Could not query dxgi factory" };
+			}
+			
+		}
+
+		void DeviceResourcesImpl::CreateDeviceForFeatureLevel()
+		{
+			for (UINT adapterIndex{ 0 }; ; ++adapterIndex)
+			{
+				DxPtr<IDXGIAdapter1> hardwareAdapter;
+				dxgiFactory->EnumAdapters1(adapterIndex, hardwareAdapter.GetAddressOf());
+				if (hardwareAdapter == nullptr)
+				{						
+					break;
+				}
+														
+				const auto result
+				{
+					D3D12CreateDevice(hardwareAdapter.Get(), minimumFeatureLevel, IID_PPV_ARGS(&device))
+				};
+				
+				if (SUCCEEDED(result) && DoesDeviceSupportAllOptions())
+				{
+					break;
+				}					
 				
 			}
+			CheckDeviceCreation();
+		
+		}
 
-			void DeviceResourcesImpl::CreateDeviceForFeatureLevel()
+			bool DeviceResourcesImpl::DoesDeviceSupportAllOptions() const
 			{
-				for (UINT adapterIndex{ 0 }; ; ++adapterIndex)
-				{
-					DxPtr<IDXGIAdapter1> hardwareAdapter;
-					dxgiFactory->EnumAdapters1(adapterIndex, hardwareAdapter.GetAddressOf());
-					if (hardwareAdapter == nullptr)
-					{						
-						break;
-					}
-															
-					const auto result
-					{
-						D3D12CreateDevice(hardwareAdapter.Get(), minimumFeatureLevel, IID_PPV_ARGS(&device))
-					};
-					
-					if (SUCCEEDED(result) && DoesDeviceSupportAllOptions())
-					{
-						break;
-					}					
-					
-				}
-				CheckDeviceCreation();
-			
+				D3D12_FEATURE_DATA_D3D12_OPTIONS options;
+				device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS, &options, sizeof(decltype(options)) );
+				
+				return options.TiledResourcesTier > D3D12_TILED_RESOURCES_TIER_NOT_SUPPORTED;
+		
 			}
 
-				bool DeviceResourcesImpl::DoesDeviceSupportAllOptions() const
+			void DeviceResourcesImpl::CheckDeviceCreation() const
+			{
+				if (!device)
 				{
-					D3D12_FEATURE_DATA_D3D12_OPTIONS options;
-					device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS, &options, sizeof(decltype(options)) );
-					
-					return options.TiledResourcesTier > D3D12_TILED_RESOURCES_TIER_NOT_SUPPORTED;
-			
+					throw Exception::CreationFailed{ "Could not create dx12 device with tiled resource support" };
 				}
-
-				void DeviceResourcesImpl::CheckDeviceCreation() const
-				{
-					if (!device)
-					{
-						throw Exception::CreationFailed{ "Could not create dx12 device with tiled resource support" };
-					}
-			
-				}
-
-
-	}
+		
+			}
 
 
 }
--- RHA/DX12/DeviceResourcesImpl.hpp
+++ RHA/DX12/DeviceResourcesImpl.hpp
@@ -2,43 +2,38 @@
 #include "Interface/DX12/DeviceResources.hpp"
 
 
-namespace RHA
+namespace RHA::DX12
 {
-	namespace DX12
+	class DeviceResourcesImpl : public DeviceResources
 	{
-		class DeviceResourcesImpl : public DeviceResources
-		{
-			private: DxPtr<IDXGIFactory4> dxgiFactory;
-			
-			private: DxPtr<ID3D12Device> device;
-			
-			private: const D3D_FEATURE_LEVEL minimumFeatureLevel;
-			
+		private: DxPtr<IDXGIFactory4> dxgiFactory;
+		
+		private: DxPtr<ID3D12Device> device;
+		
+		private: const D3D_FEATURE_LEVEL minimumFeatureLevel;
+		
 
-			
-			public: DeviceResourcesImpl(D3D_FEATURE_LEVEL minimumFeatureLevel, bool shouldEnableDebugLayers);
-			
-				private: static void EnableDebugLayers();
+		
+		public: DeviceResourcesImpl(D3D_FEATURE_LEVEL minimumFeatureLevel, bool shouldEnableDebugLayers);
+		
+			private: static void EnableDebugLayers();
 
-					private: static void CheckDebugControllerCreation(HRESULT result);
+				private: static void CheckDebugControllerCreation(HRESULT result);
 
-				private: static void CheckDxgiFactoryCreation(HRESULT result);
+			private: static void CheckDxgiFactoryCreation(HRESULT result);
 
-				private: void CreateDeviceForFeatureLevel();
+			private: void CreateDeviceForFeatureLevel();
 
-					private: bool DoesDeviceSupportAllOptions() const;
+				private: bool DoesDeviceSupportAllOptions() const;
 
-					private: void CheckDeviceCreation() const;
+				private: void CheckDeviceCreation() const;
 
-			
-			public: virtual inline DxPtr<ID3D12Device> GetDevice() override { return device; }
-			
-			public: virtual inline DxPtr<IDXGIFactory4> GetDxgiFactory() override { return dxgiFactory; }
-								
-		};
-
-
-	}
+		
+		public: virtual inline DxPtr<ID3D12Device> GetDevice() override { return device; }
+		
+		public: virtual inline DxPtr<IDXGIFactory4> GetDxgiFactory() override { return dxgiFactory; }
+							
+	};
 
 	
 }
