5ff8949e681d5c7b8e520c3706ee99dc96eabf37
Robert Bellin <rl-bellin@web.de>
2020-05-16 16:10:39 +0200

    corrected assign lights compute shader implementation

--- Content/Shaders/AssignLightsToTiles.cs
+++ Content/Shaders/AssignLightsToTiles.cs
@@ -1,3 +1,4 @@
+#include "Types/GlobalBufferData.hlsl"
 #include "Types/BoundingBox.hlsl"
 #include "Types/VolumeTileGridData.hlsl"
 #include "Types/Light.hlsl"
@@ -9,16 +10,28 @@ StructuredBuffer<Light> lights : register(t0);
 StructuredBuffer<BoundingBox> gridAABBs : register(t1);
 StructuredBuffer<uint> activeTiles : register(t2);
 
-RWBuffer<uint> relevantLightIndices : register(u2);//bound to UINT16 formatted buffer
-RWBuffer<LightIndexListInfo> offsetsToRelevantLightChunks : register(u3);
+RWBuffer<uint> relevantLightIndices : register(u1);//bound to UINT16 formatted buffer
+RWBuffer<LightIndexListInfo> offsetsToRelevantLightChunks : register(u2);
 
-
-[numthreads(4, 4, 4)]
-void main(uint3 dispatchID : SV_DispatchThreadID)
+bool LightIntersectsAABB(float lightRadius, float3 viewLightPos, BoundingBox tileBB)
 {
-	uint stride;	
-	uint numberOfTiles;
-	activeTiles.GetDimensions(numberOfTiles, stride);
+	const float3 minTile = tileBB.center - tileBB.halfExtents;
+	const float3 maxTile = tileBB.center + tileBB.halfExtents;
+	
+	const float3 minLight = viewLightPos - lightRadius;
+	const float3 maxLight = viewLightPos + lightRadius;
+	
+	bool3 r1 = minLight <= maxTile;
+	bool3 r2 = maxLight >= minTile;
+	
+	return dot(r1, r2) >= 3;
+		
+}
+
+[numthreads(32, 1, 1)]
+void main(uint3 dispatchID : SV_DispatchThreadID)
+{	
+	uint numberOfTiles = activeTiles[0]-1;
 	
 	if(dispatchID.x >= numberOfTiles)
 	{
@@ -27,17 +40,19 @@ void main(uint3 dispatchID : SV_DispatchThreadID)
 	}
 	
 	uint numberOfLights;
+	uint stride;
 	lights.GetDimensions(numberOfLights, stride);
 		
-	BoundingBox tileAABB = activeTiles[dispatchID.x]
+	BoundingBox tileAABB = gridAABBs[activeTiles[dispatchID.x+1]];
 	
 	uint intersectedLights = 0;
 	for(uint lightIndex = 0; lightIndex < numberOfLights; ++lightIndex)
 	{
-		if(LightIntersectsAABB( lights[lightIndex], tileAABB ))
+		const float3 viewLightPos = mul(view, float4(lights[lightIndex].worldPos, 1)).xyz;
+		if(LightIntersectsAABB( lights[lightIndex].radius, viewLightPos, tileAABB ))
 		{
 			relevantLightIndices[numberOfLights*dispatchID.x +intersectedLights] = lightIndex;
-			++intersectedLight;
+			++intersectedLights;
 		}				
 	}
 	
@@ -45,6 +60,6 @@ void main(uint3 dispatchID : SV_DispatchThreadID)
 	info.offset = numberOfLights * dispatchID.x;
 	info.size = intersectedLights;
 	
-	offsetsToRelevantLightChunks[activeTiles[dispatchID.x]] = info;	
+	offsetsToRelevantLightChunks[activeTiles[dispatchID.x+1]] = info;	
 	
 }
