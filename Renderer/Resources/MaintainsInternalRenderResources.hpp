b920121c9bf03a0fa5be3e4089c944734874bc04
Robert Bellin <rl-bellin@web.de>
2020-05-20 22:45:50 +0200

    fixed hidden base class function

--- Renderer/Resources/MaintainsInternalRenderResources.hpp
+++ Renderer/Resources/MaintainsInternalRenderResources.hpp
@@ -9,6 +9,8 @@ namespace Renderer
 		DEFAULTED_INTERFACE_CONSTRUCTION_OPERATIONS(MaintainsInternalRenderResources)
 
 
+		using MaintainsRenderResources::MakeBuffer;
+		
 		public: virtual size_t MakeBuffer(const void *data, size_t sizeInBytes, D3D12_RESOURCE_STATES state) = 0;
 
 		public: virtual size_t MakeUavBuffer(const void *data, size_t sizeInBytes) = 0;
