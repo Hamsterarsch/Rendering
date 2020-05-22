798aac14fca7cff2be23feb83a5ba653aea1923d
Robert Bellin <rl-bellin@web.de>
2020-05-22 14:59:39 +0200

    handle wrapper was moved out of DX12 namespace

--- Renderer/Interface/Resources/HandleWrapper.hpp
+++ Renderer/Interface/Resources/HandleWrapper.hpp
@@ -6,46 +6,40 @@ namespace Renderer
 {
 	class MaintainsRenderResources;
 		
-	namespace DX12
+	class RENDERER_DLLSPEC HandleWrapper
 	{
+		private: size_t handle;
 		
-		class RENDERER_DLLSPEC HandleWrapper
-		{
-			private: size_t handle;
-			
-			private: MaintainsRenderResources *retirementTarget;
-					 
-
-
-			public: HandleWrapper();
-			
-			public: HandleWrapper(MaintainsRenderResources *retirementTarget, size_t handle);
-
-			public: HandleWrapper(const HandleWrapper &) = delete;
-
-			public: HandleWrapper &operator=(const HandleWrapper &) = delete;
-
-			public: HandleWrapper(HandleWrapper &&other) noexcept;
-
-			public: HandleWrapper &operator=(HandleWrapper &&rhs) noexcept;
-			
-			public: void Reset();
-			
-			public: bool IsValid() const;
-			
-				private: void Invalidate();
-			
-			public: ~HandleWrapper();
-
-			
-			public: operator size_t() const { return handle; }
-
-			public: size_t Get() const { return handle; }
-			
-		};
+		private: MaintainsRenderResources *retirementTarget;
+				 
+	
+	
+		public: HandleWrapper();
+		
+		public: HandleWrapper(MaintainsRenderResources *retirementTarget, size_t handle);
+	
+		public: HandleWrapper(const HandleWrapper &) = delete;
+	
+		public: HandleWrapper &operator=(const HandleWrapper &) = delete;
+	
+		public: HandleWrapper(HandleWrapper &&other) noexcept;
+	
+		public: HandleWrapper &operator=(HandleWrapper &&rhs) noexcept;
+		
+		public: void Reset();
+		
+		public: bool IsValid() const;
+		
+			private: void Invalidate();
+		
+		public: ~HandleWrapper();
 	
 		
-	}
+		public: operator size_t() const { return handle; }
+	
+		public: size_t Get() const { return handle; }
+		
+	};
 
 	
 }
