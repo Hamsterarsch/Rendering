7dc0598c1c55a65687797be69364588780c788a1
Robert Bellin <rl-bellin@web.de>
2020-05-30 17:30:48 +0200

    modernized namespace

--- RHA/DX12/WindowSurfaceImpl.hpp
+++ RHA/DX12/WindowSurfaceImpl.hpp
@@ -6,91 +6,86 @@
 #include "DX12/CmdList.hpp"
 
 
-namespace RHA
+namespace RHA::DX12
 {
-	namespace DX12
-	{		
-		class WindowSurfaceImpl final : public WindowSurface
-		{			
-			private: static constexpr short bufferCount{ 2 };
+	class WindowSurfaceImpl final : public WindowSurface
+	{			
+		private: static constexpr short bufferCount{ 2 };
 
-			private: static constexpr float clearColor[4]{ 0,0,0,1 };
+		private: static constexpr float clearColor[4]{ 0,0,0,1 };
 
-			private: DxPtr<IDXGISwapChain1> swapChain;
+		private: DxPtr<IDXGISwapChain1> swapChain;
 
-			private: std::array<DxPtr<ID3D12Resource>, bufferCount> buffers;
+		private: std::array<DxPtr<ID3D12Resource>, bufferCount> buffers;
 
-			private: DescriptorHeapImpl viewHeap;
-					 
-			private: unsigned currentBackbufferIndex;
+		private: DescriptorHeapImpl viewHeap;
+				 
+		private: unsigned currentBackbufferIndex;
 
-			private: D3D12_VIEWPORT defaultViewport;
+		private: D3D12_VIEWPORT defaultViewport;
 
-			private: D3D12_RECT defaultRect;
+		private: D3D12_RECT defaultRect;
 
-			private: unsigned char targetedVerticalBlank;
-
-			
-					 			
-			public: WindowSurfaceImpl(class DeviceResources *resources, class Queue *queue, HWND window);
-
-				private: void CreateSwapChain(class DeviceResources *resources, class Queue *queue, HWND window);
-			
-					private: static void CheckSwapChainCreation(HRESULT result);
-
-					private: void InitializeFullscreenState();
-			
-					private: void QueryViewportInformation();
-
-				private: void CreateViewsForChainBuffers(class DeviceResources *resources);
-			
-					private: static void CheckBufferQuery(HRESULT result);
-							 				
-			public: ~WindowSurfaceImpl() override;
-
-				public: bool IsValid() const;
-
-					private: void Free();
-
-			public: WindowSurfaceImpl(WindowSurfaceImpl && other) noexcept;
-
-			public: WindowSurfaceImpl &operator=(WindowSurfaceImpl &&rhs) noexcept;
-
-				private: void Invalidate();
-			
-
-			public: size_t  GetWidth() const override { return defaultViewport.Width; }
-
-			public: size_t GetHeight() const override { return defaultViewport.Height; }
-
-			public: void EnableVerticalSync() override { targetedVerticalBlank = 1; }
-
-			public: void DisableVerticalSync() override { targetedVerticalBlank = 0; }
-			
-			public: DxPtr<ID3D12Resource> GetResourceTemplate() override { return buffers[0]; }
-																	   			
-			public: void Present() override;
-
-			public: void RecordPipelineBindings(CmdList &list, const D3D12_CPU_DESCRIPTOR_HANDLE *depthDescriptor) override;
-
-				private: ID3D12Resource *GetBackbuffer();
-
-			public: void RecordPreparationForRendering(CmdList &list) override;
-
-			public: void RecordPreparationForPresenting(CmdList &list) override;
-
-			
-
-			public: void GoFullscreen() override;
-
-			public: void ResizeToWindow() override;
-			
-			public: void GoWindowed() override;
-
-		};
+		private: unsigned char targetedVerticalBlank;
 
 		
-	}
+				 			
+		public: WindowSurfaceImpl(class DeviceResources *resources, class Queue *queue, HWND window);
+
+			private: void CreateSwapChain(class DeviceResources *resources, class Queue *queue, HWND window);
+		
+				private: static void CheckSwapChainCreation(HRESULT result);
+
+				private: void InitializeFullscreenState();
+		
+				private: void QueryViewportInformation();
+
+			private: void CreateViewsForChainBuffers(class DeviceResources *resources);
+		
+				private: static void CheckBufferQuery(HRESULT result);
+						 				
+		public: ~WindowSurfaceImpl() override;
+
+			public: bool IsValid() const;
+
+				private: void Free();
+
+		public: WindowSurfaceImpl(WindowSurfaceImpl && other) noexcept;
+
+		public: WindowSurfaceImpl &operator=(WindowSurfaceImpl &&rhs) noexcept;
+
+			private: void Invalidate();
+		
+
+		public: size_t  GetWidth() const override { return defaultViewport.Width; }
+
+		public: size_t GetHeight() const override { return defaultViewport.Height; }
+
+		public: void EnableVerticalSync() override { targetedVerticalBlank = 1; }
+
+		public: void DisableVerticalSync() override { targetedVerticalBlank = 0; }
+		
+		public: DxPtr<ID3D12Resource> GetResourceTemplate() override { return buffers[0]; }
+																   			
+		public: void Present() override;
+
+		public: void RecordPipelineBindings(CmdList &list, const D3D12_CPU_DESCRIPTOR_HANDLE *depthDescriptor) override;
+
+			private: ID3D12Resource *GetBackbuffer();
+
+		public: void RecordPreparationForRendering(CmdList &list) override;
+
+		public: void RecordPreparationForPresenting(CmdList &list) override;
+
+		
+
+		public: void GoFullscreen() override;
+
+		public: void ResizeToWindow() override;
+		
+		public: void GoWindowed() override;
+
+	};
 	
 	
 }
