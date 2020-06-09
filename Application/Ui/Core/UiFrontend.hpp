49111e8cae10bc605ff722bf69883f0e89a862e9
Robert Bellin <rl-bellin@web.de>
2020-06-09 14:43:05 +0200

    added uiFrontend interface

--- Application/Ui/Core/UiFrontend.hpp
+++ Application/Ui/Core/UiFrontend.hpp
@@ -4,11 +4,19 @@
 
 namespace App::Ui
 {
+	class StringInputTarget;
+	
 	class UiFrontend
 	{
 		DEFAULTED_INTERFACE_CONSTRUCTION_OPERATIONS(UiFrontend)
 		
+
+		public: virtual void Update() = 0;
 		
+		public: virtual StringInputTarget *GetInputTargetString(size_t index) { return nullptr; }
+
+		public: virtual bool *GetInputTargetBool(size_t index) { return nullptr; }
+					
 	};
 		
 	
