aad68b20321fac6b9c938d33b31678bd116aaf73
Robert Bellin <rl-bellin@web.de>
2020-07-02 13:42:57 +0200

    removed functions from the core uiLayoutElement interface

--- Application/Ui/Core/UiLayoutElement.hpp
+++ Application/Ui/Core/UiLayoutElement.hpp
@@ -7,23 +7,9 @@ namespace App::Ui::Core
 {	
 	class UiLayoutElement : public UiElement
 	{				
-		public: virtual void ClearChildren() = 0;
-		
-		protected: virtual size_t GetChildCount() const = 0;
-				
 		public: virtual void AddChild(UniquePtr<UiElement> &&child) = 0;
-
-		protected: virtual void OnChildAdded(UiElement &child) = 0;
-							   				 
-		public: virtual void RenderAndQueryInternal(UiBuilder &builder) = 0;
-
-		protected: virtual void OnPreRenderAndQueryChildren(UiBuilder &builder) = 0;
-		
-		protected: virtual void OnPreRenderAndQueryChild(UiBuilder &builder, size_t childIndex, UiElement &child) = 0;
-
-		protected: virtual void OnPostRenderAndQueryChild(UiBuilder &builder, size_t childIndex, UiElement &child) = 0;
-
-		protected: virtual void OnPostRenderAndQueryChildren(UiBuilder &builder) = 0;
+				
+		public: virtual void ClearChildren() = 0;
 		
 	};
 
--- Application/Ui/Core/UiLayoutElementBasic.hpp
+++ Application/Ui/Core/UiLayoutElementBasic.hpp
@@ -12,22 +12,22 @@ namespace App::Ui::Core
 
 
 		public: void ClearChildren() final override { children.clear(); }
-		
-		protected: size_t GetChildCount() const final override { return children.size(); }
-				
+						
 		public: void AddChild(UniquePtr<UiElement> &&child) override;
 
-		protected: virtual void OnChildAdded(UiElement &child) override{}
-							   				 
 		public: void RenderAndQueryInternal(UiBuilder &builder) final override;
+			
+		protected: virtual void OnChildAdded(UiElement &child) {}
 
-		protected: virtual void OnPreRenderAndQueryChildren(UiBuilder &builder) override{}
+		protected: virtual void OnPreRenderAndQueryChildren(UiBuilder &builder) {}
 		
-		protected: virtual void OnPreRenderAndQueryChild(UiBuilder &builder, size_t childIndex, UiElement &child) override{}
+		protected: virtual void OnPreRenderAndQueryChild(UiBuilder &builder, size_t childIndex, UiElement &child) {}
 
-		protected: virtual void OnPostRenderAndQueryChild(UiBuilder &builder, size_t childIndex, UiElement &child) override{}
+		protected: virtual void OnPostRenderAndQueryChild(UiBuilder &builder, size_t childIndex, UiElement &child) {}
 
-		protected: virtual void OnPostRenderAndQueryChildren(UiBuilder &builder) override{}
+		protected: virtual void OnPostRenderAndQueryChildren(UiBuilder &builder) {}
+		
+		protected: size_t GetChildCount() const { return children.size(); }
 		
 	};
 
