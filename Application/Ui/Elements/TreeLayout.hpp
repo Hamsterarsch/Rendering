1612f8f23ab4a822fff84625f59552f3af7d3ef7
Robert Bellin <rl-bellin@web.de>
2020-07-02 13:44:45 +0200

    added makeshift tree implementation

--- Application/Ui/Elements/TreeLayout.hpp
+++ Application/Ui/Elements/TreeLayout.hpp
@@ -1,30 +1,72 @@
 #pragma once
-#include "Ui/Core/UiLayoutElement.hpp"
 
 #include "Ui/Core/UiBuilder.hpp"
+#include <string>
+#include "Ui/Core/UiLayoutElementBasic.hpp"
+#include "Ui/Core/UiFrontend.hpp"
 
 
 namespace App::Ui
 {
-	class TreeNode
-	{
-		std::string displayName;
-		UniquePtr<Core::UiElement> element;
-		
-	};
+	class TreeLayoutFrontend;
 	
 	class TreeLayout final : public Core::UiLayoutElement
 	{
-		public: std::string nextDisplayName;
+		private: inline static constexpr float headerSpacing{ 5 };
+		
+		private: UniquePtr<UiElement> headerElement;
 
+		private: UniquePtr<UiElement> child;
+
+		private: float indent;
 
 		
-		protected: void OnChildAdded(UiElement &child) override
-		{
-			
-		}
-				
+
+		public: TreeLayout(Core::UiFrontend &frontend, size_t isOpenIndex, const char *header, float indent = 10);
+
+		public: TreeLayout(TreeLayoutFrontend &frontend, const char *header, float indent = 10);
+		
+		
+		public: void AddChild(UniquePtr<UiElement> &&child) override;
+				   
+		public: void ClearChildren() override;
+
+		protected: void RenderAndQueryInternal(Core::UiBuilder &builder) override;
+
+		public: void SetChildIsHidden(bool value) { child->SetIsHidden(value); }
+				   		
 	};
 
-	
+
+
+	class TreeLayoutFrontend final : public Core::UiFrontend
+	{
+		private: bool isHeaderNodeToggledOn;
+
+		private: TreeLayout *targetTree;
+		
+
+		public: TreeLayoutFrontend() : isHeaderNodeToggledOn{ true }, targetTree{ nullptr } {}
+
+		public: void SetTargetTree(TreeLayout &tree) { targetTree = &tree; }
+		
+		public: void Update(Core::UiBuilder &builder) override
+		{
+			if(targetTree)
+			{
+				targetTree->SetChildIsHidden(not isHeaderNodeToggledOn);				
+			}
+						
+		}
+
+
+		bool *GetInputTargetBool(size_t index) override
+		{
+			return &isHeaderNodeToggledOn;
+			
+		}
+		
+	};
+		
+		
 }
