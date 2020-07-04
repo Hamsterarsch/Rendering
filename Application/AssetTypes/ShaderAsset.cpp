45cb31dccb1da1aefeedc98fd37298b19766ef27
Robert Bellin <rl-bellin@web.de>
2020-07-04 19:23:39 +0200

    shader assets now contain resource slot declarations and the shader editor can manipulate those. Resource slots are displayed in a tree-like manner

--- Application/AssetTypes/ShaderAsset.cpp
+++ Application/AssetTypes/ShaderAsset.cpp
@@ -4,22 +4,70 @@
 
 namespace App::Assets
 {
-	ShaderAsset::ShaderAsset() {}
-	
+	assetSystem::io::Archive &ConstantsResourceSlot::Serialize(assetSystem::io::Archive &archive)
+	{		
+		return archive
+		.Serialize("register", shaderRegister)
+		.Serialize("size", sizeInVector4);
+		
+	}
 
-	assetSystem::io::Archive &ShaderAsset::Serialize(assetSystem::io::Archive &archive)
+
+
+
+	assetSystem::io::Archive &TextureResourceSlot::Serialize(assetSystem::io::Archive &archive)
 	{
-		if(archive.IsReading())
-		{
-			shaderCode.resize(archive.GetPropertySizeInBytes("code"));
-		}
-				
-		return archive.Serialize("code", shaderCode.data());
+		return archive
+		.Serialize("register", shaderRegister);
 		
 	}
 
 
 	
+	
+	assetSystem::io::Archive &ShaderAsset::Serialize(assetSystem::io::Archive &archive)
+	{
+		if(archive.IsReading())
+		{
+			shaderCode.resize(archive.GetPropertySizeInBytes("code"));
+		}				
+		archive.Serialize("code", shaderCode.data());
+
+		SerializeResourceSlots(archive);
+		
+		return archive;
+		
+	}
+
+		void ShaderAsset::SerializeResourceSlots(assetSystem::io::Archive& archive)
+		{
+			auto numCbvSlots{ static_cast<unsigned>(constantsSlots.size()) };
+			archive.Serialize("numConstantsSlots", numCbvSlots);
+	
+			auto numTexSlots{ static_cast<unsigned>(textureSlots.size()) };
+			archive.Serialize("numTextureSlots", numTexSlots);
+						
+			if(archive.IsReading())
+			{
+				constantsSlots.resize(numCbvSlots);
+				textureSlots.resize(numTexSlots);
+			}
+
+
+			for(unsigned slotIndex{ 0 }; slotIndex < numCbvSlots; ++slotIndex)
+			{
+				archive.Serialize(("cbvSlot"+std::to_string(slotIndex)).c_str(), constantsSlots.at(slotIndex));
+			}
+
+			for(unsigned slotIndex{ 0 }; slotIndex < numTexSlots; ++slotIndex)
+			{
+				archive.Serialize(("texSlot"+std::to_string(slotIndex)).c_str(), textureSlots.at(slotIndex));
+			}
+		
+		}
+
+
+
 	const char *ShaderAsset::GetAssetClassExtension()
 	{
 		static const char *extension{ "shdr" };
--- Application/AssetTypes/ShaderAsset.hpp
+++ Application/AssetTypes/ShaderAsset.hpp
@@ -1,21 +1,66 @@
 #pragma once
 #include "Asset.hpp"
 #include <string>
+#include <vector>
+
 
 
 namespace App::Assets
 {
+	class ConstantsResourceSlot final : public assetSystem::Archivable
+	{
+		public: unsigned shaderRegister;
+		
+		public: unsigned sizeInVector4;
+
+		
+
+		public: ConstantsResourceSlot() : ConstantsResourceSlot{ 0, 0 } {}
+		
+		public: ConstantsResourceSlot(unsigned shaderRegister, unsigned sizeInVector4) : shaderRegister{ shaderRegister }, sizeInVector4{ sizeInVector4 } {}
+		
+		public: assetSystem::io::Archive &Serialize(assetSystem::io::Archive &archive) override;
+		
+	};
+
+
+	
+	
+	class TextureResourceSlot final : public assetSystem::Archivable
+	{
+		public: unsigned shaderRegister;
+
+		
+
+		public: TextureResourceSlot() : TextureResourceSlot{ 0 } {}
+		
+		public: TextureResourceSlot(unsigned shaderRegister) : shaderRegister{ shaderRegister } {}
+		
+		public: assetSystem::io::Archive &Serialize(assetSystem::io::Archive &archive) override;
+				
+	};
+
+
+
+	
 	class ShaderAsset final : public assetSystem::Asset
 	{
 		private: std::string shaderCode;
 
+		private: std::vector<ConstantsResourceSlot> constantsSlots;
+
+		private: std::vector<TextureResourceSlot> textureSlots;
 
 
-		public: ShaderAsset();
+		
+		public: ShaderAsset() = default;
 
 		public: ShaderAsset(const char *code) : shaderCode{ code } {}
 
+						
+		public: assetSystem::io::Archive &Serialize(assetSystem::io::Archive &archive) override;
 
+			private: void SerializeResourceSlots(assetSystem::io::Archive &archive);
 		
 		public: const char *GetCode() const { return shaderCode.c_str(); }
 
@@ -23,7 +68,19 @@ namespace App::Assets
 
 		public: void SetCode(const std::string &code) { shaderCode = code; }
 
-		public: assetSystem::io::Archive &Serialize(assetSystem::io::Archive &archive) override;
+		public: void ClearResourceSlots() { constantsSlots.clear(); textureSlots.clear(); }
+		
+		public: void AddConstantsSlot(const ConstantsResourceSlot &slot) { constantsSlots.emplace_back(slot); }
+
+		public: size_t GetNumConstantsSlots() const { return constantsSlots.size(); }
+
+		public: ConstantsResourceSlot GetConstantsSlotAt(const size_t index) const { return constantsSlots.at(index); }
+		
+		public: void AddTextureSlot(const TextureResourceSlot &slot) { textureSlots.emplace_back(slot); }
+
+		public: size_t GetNumTextureSlots() const { return textureSlots.size(); }
+
+		public: TextureResourceSlot GetTextureSlotAt(const size_t index) const { return textureSlots.at(index); }
 				
 		public: static const char *GetAssetClassExtension();
 
