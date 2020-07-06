79f436f3bf1b8816a1b5b2f6040b03a16c76874b
Robert Bellin <rl-bellin@web.de>
2020-07-06 13:28:01 +0200

    added error message querying to rha shader factory

--- RHA/DX12/ShaderFactoryImpl.hpp
+++ RHA/DX12/ShaderFactoryImpl.hpp
@@ -4,61 +4,71 @@
 #include "IncludeHandler.hpp"
 
 
-namespace RHA
+namespace RHA::DX12
 {
-	namespace DX12
+	class ShaderFactoryImpl final : public ShaderFactory
 	{
-		class ShaderFactoryImpl : public ShaderFactory
-		{
-			private: std::string shaderModelSpec;
-			
-			private: bool insertDebugInfo;
-			
-			private: bool skipOptimization;
-			
-			private: bool useHighOptimization;
-
-			private: IncludeHandler includeHandler;
-
-
-			
-			public: ShaderFactoryImpl(unsigned char shaderModelMajor, unsigned char shaderModelMinor);
-			
-			public: virtual void SkipOptimization(bool value) override;
-
-			public: virtual void AddDebugInfo(bool value) override;
-
-			public: virtual void AllowHighOptimizationTimes(bool value) override;
-
-			public: virtual void AddIncludeDirectory(const char *directory) override;
-
-								
-			public: virtual DxPtr<ID3DBlob> MakeVertexShader(const wchar_t *filepath, const char *entrypoint) override;
-
-				private: DxPtr<ID3DBlob> DoCompileFromFile(const wchar_t *filepath, const char *entrypoint, const char *shaderTypePrefix);
+		private: std::string shaderModelSpec;
 		
-					private: UINT MakeCompileFlags() const;
+		private: bool insertDebugInfo;
+		
+		private: bool skipOptimization;
+		
+		private: bool useHighOptimization;
 
-				private: static void CheckCompilationFromFile(HRESULT result, const DxPtr<ID3DBlob> &errorBlob);
+		private: IncludeHandler includeHandler;
 
-			public: virtual DxPtr<ID3DBlob> MakeVertexShader(const char *shader, size_t shaderLength, const char *entrypoint) override;
+		private: std::string lastCompileMessage;
 
-				private: DxPtr<ID3DBlob> DoCompile(const char *shader, size_t shaderLength, const char *entrypoint, const char *shaderTypePrefix);
+		private: bool compileFailed;
 
-					private: static void CheckCompilation(HRESULT result, const DxPtr<ID3DBlob> &errorBlob);
-			
-			
-			public: virtual DxPtr<ID3DBlob> MakePixelShader(const wchar_t *filepath, const char *entrypoint) override;
+		private: bool compiledWithWarnings;
 
-			public: virtual DxPtr<ID3DBlob> MakePixelShader(const char *shader, size_t shaderLength, const char *entrypoint) override;
-
-			
-			public: virtual DxPtr<ID3DBlob> MakeComputeShader(const char* shader, size_t shaderLength, const char* entrypoint) override;
-			
-		};
 
 		
-	}
+		public: ShaderFactoryImpl(unsigned char shaderModelMajor, unsigned char shaderModelMinor);
+		
+		public: void SkipOptimization(bool value) override;
+
+		public: void AddDebugInfo(bool value) override;
+
+		public: void AllowHighOptimizationTimes(bool value) override;
+
+		public: void AddIncludeDirectory(const char *directory) override;
+
+							
+		public: DxPtr<ID3DBlob> MakeVertexShader(const wchar_t *filepath, const char *entrypoint) override;
+
+			private: DxPtr<ID3DBlob> DoCompileFromFile(const wchar_t *filepath, const char *entrypoint, const char *shaderTypePrefix);
+	
+				private: UINT MakeCompileFlags() const;
+
+				private: void CheckCompilation(HRESULT result, const DxPtr<ID3DBlob> &errorBlob);
+		
+
+		public: DxPtr<ID3DBlob> MakeVertexShader(const char *shader, size_t shaderLength, const char *entrypoint) override;
+
+			private: DxPtr<ID3DBlob> DoCompile(const char *shader, size_t shaderLength, const char *entrypoint, const char *shaderTypePrefix);
+
+		
+		
+		public: DxPtr<ID3DBlob> MakePixelShader(const wchar_t *filepath, const char *entrypoint) override;
+
+		public: DxPtr<ID3DBlob> MakePixelShader(const char *shader, size_t shaderLength, const char *entrypoint) override;
+
+		
+		public: DxPtr<ID3DBlob> MakeComputeShader(const char* shader, size_t shaderLength, const char* entrypoint) override;
+
+		
+		public: bool WasCompileSuccessful() const override { return not compileFailed; }
+		
+		public: std::string GetCompileMessage() const override { return lastCompileMessage; }
+		
+		public: bool WasCompiledWithWarnings() const override{ return compiledWithWarnings; }
+		
+	};
+
+		
+}
 
 	
-}
