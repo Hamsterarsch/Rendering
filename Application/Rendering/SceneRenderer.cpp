8ee423333a6647c1a6de45514a622902c57ea2aa
Robert Bellin <rl-bellin@web.de>
2020-07-07 14:10:47 +0200

    changed scene renderer construction

--- Application/Rendering/SceneRenderer.cpp
+++ Application/Rendering/SceneRenderer.cpp
@@ -1,7 +1,5 @@
 #include "Rendering/SceneRenderer.hpp"
 #include "Utility.hpp"
-#include "Resources/HandleWrapper.hpp"
-#include "Core/Application.hpp"
 #include "AssetPtr.hpp"
 #include "AssetTypes/ShaderAsset.hpp"
 #include "Shared/Debugging.hpp"
@@ -10,43 +8,9 @@
 
 namespace App::Rendering
 {
-	struct PipelineInfo
-	{
-		Renderer::HandleWrapper signature;
-		Renderer::HandleWrapper pso;
-	};
 	
-	PipelineInfo MakePsoVolumeTileGridCreationPipeline(assetSystem::AssetSystem &shaderProvider, Renderer::RendererFacade &renderer)
-	{
-		PipelineInfo out;
-		
-		//load all necessary shader assets and compile their code here
-		assetSystem::AssetPtrTyped<Assets::ShaderAsset> csCreateVolumeTileGrid{ shaderProvider.GetAsset("Shaders/CreateVolumeTileGrid.cs.shdr") };
+	
 
-		csCreateVolumeTileGrid->Compile(renderer);
-		if(not renderer.WasCompileSuccessful())
-		{
-			Exception::DebugBreak();
-		}
-				
-		//make a signature with the required slots
-		auto &settings{ renderer.GetSignatureSettings() };
-		settings.RestoreSettings();
-
-		settings
-		.DeclareTable()
-		.AddTableRange(&Renderer::DescriptorTargets::ConstantBuffer, 0, 1)
-		.AddTableRange(&Renderer::DescriptorTargets::UnorderedAccess, 0, 1);
-
-		Renderer::SerializeTarget rootData;
-		renderer.SerializeRootSignature(rootData, nullptr, 0);
-		out.signature = { &renderer, renderer.MakeRootSignature(rootData.GetData(), rootData.GetSizeInBytes(), 0) };
-		
-		out.pso = { &renderer, renderer.MakePso(Renderer::Blob{csCreateVolumeTileGrid->GetCompiledCode(), csCreateVolumeTileGrid->GetCompiledCodeSizeInBytes()}, out.signature) };
-
-		return out;
-		
-	}
 	
 	
 	SceneRenderer::SceneRenderer
@@ -57,11 +21,39 @@ namespace App::Rendering
 		const Math::VectorUint2 &surfaceDimensions
 	)	:
 		mediator{ &mediator },
-		projection{ Math::Matrix::MakeProjection(Math::Radians(90), surfaceDimensions.x, surfaceDimensions.y, 100, 50'000) }
-	{
-		MakePsoVolumeTileGridCreationPipeline(internalShaderProvider, renderer);
-		
+		projection{ Math::Matrix::MakeProjection(Math::Radians(90), surfaceDimensions.x, surfaceDimensions.y, 100, 50'000) },
+		createVolumeTileGridPipeline{ MakePsoVolumeTileGridCreationPipeline(internalShaderProvider, renderer) }
+	{		
 	}
+
+		PipelineData SceneRenderer::MakePsoVolumeTileGridCreationPipeline(assetSystem::AssetSystem &shaderProvider, Renderer::RendererFacade &renderer)
+		{						
+			assetSystem::AssetPtrTyped<Assets::ShaderAsset> csCreateVolumeTileGrid{ shaderProvider.GetAsset("Shaders/CreateVolumeTileGrid.cs.shdr") };		
+			csCreateVolumeTileGrid->Compile(renderer);
+			if(not renderer.WasCompileSuccessful())
+			{
+				Exception::DebugBreak();
+			}					
+
+		
+			auto &settings{ renderer.GetSignatureSettings() };
+			settings.RestoreSettingsToSaved();		
+			settings
+			.DeclareTable()
+			.AddTableRange(&Renderer::DescriptorTargets::ConstantBuffer, 0, 1)
+			.AddTableRange(&Renderer::DescriptorTargets::UnorderedAccess, 0, 1);
+		
+			PipelineData out;		
+			Renderer::SerializeTarget rootData;
+			renderer.SerializeRootSignature(rootData, nullptr, 0);
+			out.signature = { &renderer, renderer.MakeRootSignature(rootData.GetData(), rootData.GetSizeInBytes(), 0) };
+
+		
+			out.pso = { &renderer, renderer.MakePso(Renderer::Blob{csCreateVolumeTileGrid->GetCompiledCode(), csCreateVolumeTileGrid->GetCompiledCodeSizeInBytes()}, out.signature) };
+		
+			return out;
+			
+		}
 
 
 
@@ -69,6 +61,8 @@ namespace App::Rendering
 	{
 	}
 
+
+	
 	void SceneRenderer::OnProjectionChanged()
 	{
 	}
--- Application/Rendering/SceneRenderer.hpp
+++ Application/Rendering/SceneRenderer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Math/Interface/Types/Matrix.hpp"
+#include "Resources/HandleWrapper.hpp"
 
 
 namespace assetSystem { class AssetSystem; }
@@ -9,7 +10,13 @@ namespace Renderer{ class RendererFacade; }
 namespace App::Rendering
 {
 	class RendererMediator;
-	
+
+	struct PipelineData
+	{
+		Renderer::HandleWrapper signature;
+		Renderer::HandleWrapper pso;
+	};
+		
 	class SceneRenderer
 	{
 		private: RendererMediator *mediator;
@@ -18,6 +25,7 @@ namespace App::Rendering
 
 		private: Math::Matrix view;
 
+		private: PipelineData createVolumeTileGridPipeline;
 		
 		
 		public: SceneRenderer
@@ -28,6 +36,9 @@ namespace App::Rendering
 			const Math::VectorUint2 &surfaceDimensions
 		);
 
+			private: static PipelineData MakePsoVolumeTileGridCreationPipeline(assetSystem::AssetSystem &shaderProvider, Renderer::RendererFacade &renderer);
+		
+		
 		public: void SubmitFrame();
 		
 		public: void SetCamera(const Math::Vector3 &position, const Math::Vector3 &rotation);
