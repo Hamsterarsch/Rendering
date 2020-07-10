c35839e5ef2dafb8caa059770eefe22e1e5c73f5
Robert Bellin <rl-bellin@web.de>
2020-07-10 15:34:27 +0200

    added user pixel shader template asset

--- Application/AssetTypes/PsoAsset.hpp
+++ Application/AssetTypes/PsoAsset.hpp
@@ -2,8 +2,6 @@
 #include "Asset.hpp"
 #include "Resources/HandleWrapper.hpp"
 
-#include "Core/Application.hpp"
-#include "AssetSystem.hpp"
 
 namespace App::Assets
 {
@@ -17,17 +15,6 @@ namespace App::Assets
 		
 		public: assetSystem::io::Archive& Serialize(assetSystem::io::Archive& archive) override { return archive; }
 	};
-
-	Renderer::HandleWrapper MakePsoVolumeTileGridCreation(Core::Application &app)
-	{
-		//load all necessary shader assets and compile their code here
-		app.GetProgramAssets().GetAsset("Shaders/")
-
-		
-		//make a signature with the required slots
-
-		//make the pso
-		
-	}
+	
 	
 }
